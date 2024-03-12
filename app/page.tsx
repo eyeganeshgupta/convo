06788f1d53600bb0c9ce71f3e62dd3e465ac4c8a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-12 09:25:52 +0530

    project-setup🛠️

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-bold text-indigo-500">Convo</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
