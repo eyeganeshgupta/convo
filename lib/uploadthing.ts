8d709d73897bc57180bf434609e893ac844f14c7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-15 09:12:40 +0530

    settingUp-uploadthing🛠️

---
+++ lib/uploadthing.ts
@@ -0,0 +1,16 @@
+import { OurFileRouter } from "@/app/api/uploadthing/core";
+import { generateComponents } from "@uploadthing/react";
+/*
+import {
+    generateUploadButton,
+    generateUploadDropzone,
+} from "@uploadthing/react";
+*/
+
+/*
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+*/
+
+export const { UploadButton, UploadDropzone, Uploader } =
+  generateComponents<OurFileRouter>();
